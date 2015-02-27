9bb6416fa3fb86ee6d9729b8159abad144b6e1e0
Albin Olsson <albin.olsson@gmail.com>
2015-02-27 19:11:15 +0100

    Bump copyright year

--- xcape.c
+++ xcape.c
@@ -1,7 +1,7 @@
 /************************************************************************
  * xcape.c
  *
- * Copyright 2012 Albin Olsson
+ * Copyright 2015 Albin Olsson
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
